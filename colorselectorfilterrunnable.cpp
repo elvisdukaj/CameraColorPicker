3823d8e850f0e0360d3ce158ca59f97b25f062b5
elvis dukaj <elvis dukaj>
2016-03-03 00:26:24 +0100

    QVideoFrame crash se QImage ha come formato Format_RGB32

--- colorselectorfilterrunnable.cpp
+++ colorselectorfilterrunnable.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/opencv.hpp>
 #include <QDebug>
 
+cv::Mat frame;
+
 ColorSelectorFilterRunnable::
 ColorSelectorFilterRunnable(ColorSelectorFilter *filter)
     : m_Filter( filter )
@@ -24,28 +26,19 @@ QVideoFrame ColorSelectorFilterRunnable::run(QVideoFrame *input, const QVideoSur
             return *input;
 
         cv::Mat frameRGBA(input->height(), input->width(), CV_8UC4, input->bits(), input->bytesPerLine());
-        cv::Mat frame = frameRGBA.clone();
+        frame = frameRGBA.clone();
         input->unmap();
 
         frame.convertTo(frame, CV_8UC3);
 
         cv::flip(frame, frame, -1);
 
-
-//        cv::cvtColor(frame, frame, cv::COLOR_BGR2RGBA);
-
-
-        cv::imshow("frame", frame);
-        cv::imshow("frameRGBA", frameRGBA);
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2RGBA);
 
 
-//        cv::imshow("frame", frame);
-//        cv::imshow("frameRGBA", frameRGBA);
-//        QImage image(frameRGBA.data, frameRGBA.cols, frameRGBA.rows, QImage::Format_RGB32);
-
-        QImage image(frame.data, frameRGBA.cols, frameRGBA.rows, frameRGBA.step, QImage::Format_RGB32);
-//        image.save("image.bmp");
-        return QVideoFrame(image);
+        QImage image(frame.data, frame.cols, frame.rows, frame.step, QImage::Format_RGB888);
+        QVideoFrame vframe(image);
+        return vframe;
     }
 
     return *input;
