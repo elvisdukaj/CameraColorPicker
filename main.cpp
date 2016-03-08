23852beeb3ac8eccfb9988e7bbf91b7ba3fe2483
elvis dukaj <elvis.dukaj@gmail.com>
2016-03-08 09:38:48 +0100

    possibilita' di scegliere fra QQuickView e QQmlApplicationEngine

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include "colorselectorfilter.h"
+
+#define USE_QML_APPLICATION_ENGINE
+
+#ifdef USE_QML_APPLICATION_ENGINE
 #include <QtQuick>
+#else
+#include <QtGui/QGuiApplication>
+#include <QtCore/QDir>
+#include <QtQuick/QQuickView>
+#include <QtQml/QQmlEngine>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -7,9 +17,13 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<ColorSelectorFilter>("Elice.CameraColorSelector", 1, 0, "ColorFilter");
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-
+#ifdef USE_QML_APPLICATION_ENGINE
+	QQmlApplicationEngine engine;
+	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+#else
+	QQuickView viewer;
+	viewer.setSource(QUrl("qrc:/main.qml"));
+#endif
 
     return app.exec();
 }
